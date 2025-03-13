3d9400a626a6ff2b6061bd5395ddc387c81996c0
Felix Patschkowski <felix@patschkowski.net>
2025-03-13 16:11:34 +0800

    Avoid warning

--- libhello/hello.test.cpp
+++ libhello/hello.test.cpp
@@ -1,9 +1,18 @@
 #define BOOST_TEST_MODULE libhello Tests
 #include <boost/test/included/unit_test.hpp>
 
+#ifdef _MSC_VER
+#pragma warning push
+#pragma warning(disable : 26426)
+#endif
+
 import hello;
 
 BOOST_AUTO_TEST_SUITE(libhello_tests)
+
+#ifdef _MSC_VER
+#pragma warning pop
+#endif
 
 BOOST_AUTO_TEST_CASE(say_hello_greets_name)
 {
